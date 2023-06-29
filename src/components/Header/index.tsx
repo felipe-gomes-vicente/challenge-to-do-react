10942f287896006db442887312399d8b5aeb84d8
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-06-29 17:40:52 -0300

    styles component Header and install lib react-icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,21 @@
+import { AiOutlinePlusCircle } from 'react-icons/ai'
+
 import todoLogo from './../../assets/todoLogo.svg'
+
+import styles from './header.module.css'
 
 export function Header() {
   return (
-    <header>
+    <header className={styles.header} >
       <img src={todoLogo} alt="" />
+
+      <form className={styles.newTaskForm}>
+        <input type="text" placeholder='Adicione uma nova tarefa' />
+        <button>
+          Criar 
+          <AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
     </header>
   )
 }
