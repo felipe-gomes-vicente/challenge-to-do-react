9322f68e87980323246a404c4775aeecbe92dbd9
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-06-30 14:15:58 -0300

    created function addTask, type Props, function handleSubmit,, function onChangeTitle and states

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,35 @@
 import { AiOutlinePlusCircle } from 'react-icons/ai'
+import { useState, FormEvent, ChangeEvent } from 'react';
 
 import todoLogo from './../../assets/todoLogo.svg'
 
 import styles from './header.module.css'
 
-export function Header() {
+interface Props {
+  onAddTask: (taskTitle: string) => void;
+}
+
+export function Header({ onAddTask }: Props) {
+  const [title, setTitle] = useState("")
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    onAddTask(title)
+    setTitle("")
+    
+  }
+
+  function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value)
+  }
+
   return (
     <header className={styles.header} >
       <img src={todoLogo} alt="" />
 
-      <form className={styles.newTaskForm}>
-        <input type="text" placeholder='Adicione uma nova tarefa' />
+      <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+        <input type="text" onChange={onChangeTitle} value={title} placeholder='Adicione uma nova tarefa' />
         <button>
           Criar 
           <AiOutlinePlusCircle size={20} />
